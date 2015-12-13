42cf05130c8207887ce13c057a852a007f90067b
stephanieyang <stephanie.yang.121@gmail.com>
2015-12-12 23:01:03 -0500

    tentatively done

--- final.js
+++ final.js
@@ -1,33 +1,102 @@
-
-
-function highlightCyan(target) {
-	console.log("highlightCyan, target = " + target);
-	console.log($(target));
-	$(target).css("background-color","#00FFFF");
-}
-
-function revertHighlight(target) {
-	console.log("revertHighlight, target = " + target);
-	console.log($(target));
-	$(target).css("background-color","transparent");
-}
-
+/* Turns line highlighting on/off. */
 function toggleColor(target, color) {
-	console.log("toggleColor");
-	console.log($(target));
 	$(target).toggleClass(color);
 }
 
-function toggleUnderline(target, color) {
-	console.log("toggleUnderline");
-	console.log($(target));
-	$(target).toggleClass("underline-" + color);
+/* Reveals text in a certain textbox when a given button is clicked. The button is disabled afterwards. */
+function showValueOnClick(buttonName, boxName, value) {
+	$(boxName).val(value);
+	$(buttonName).addClass("disabled");
 }
 
+/* Constructor for answers to assessment problems.
+ * Kind of hack-y (this requires separate maintenance of problem definitions and problem answers)
+ * but taking the problem definitions from code and accounting for all the CSS/formatting manipulation
+ * would take longer than is feasible for this project. Alas.
+ */
+function ProblemAnswer(options) {
+	var options = options ? options : {};
+
+	this.solnLineArray = options.solnLineArray ? options.solnLineArray : [];
+	this.freeResponseArray = options.freeResponseArray ? options.freeResponseArray : [];
+}
+
+/* Checks user answers to assessment problems and returns a list of any errors the user has made.
+ * Assumes all lines in a function are unique. This won't be true for any arbitrary code
+ * (notably this might be buggy on two-function problems for global highlighting due to the multiple '}' lines),
+ * but is good enough for the purposes of this project. */
+function getProblemErrors(problemNumber) {
+		var currentProblem = assessmentProblems[problemNumber-1];
+		var errors = [];
+
+		var highlightedLines = $("#try-" + problemNumber + " .well .yellow"); // array of span elements
+		var numHighlightedLines = highlightedLines.length;
+		var wantedLines = currentProblem.solnLineArray;
+		var numWantedLines = wantedLines.length;
+		var numMatchedLines = 0;
+		var overHighlighted = false;
+		var underHighlighted = false;
+
+		/* Checking highlights */
+		/*
+		iterate through highlightedLines:
+			if in wantedLines:
+				increase counter
+			else:
+				add error: added at least one line too many
+		if counter < wantedLines: this means at least one wanted line has not been highlighted
+			add error: missing at least one line
+		output list of errors (may be empty or non-empty depending on user errors)
+		*/
+		for(var i = 0; i < numHighlightedLines; i++) {
+			var matchFound = false;
+			for(var j = 0; j < numWantedLines; j++) {
+				if($(highlightedLines[i]).text() === wantedLines[j]) {
+					//console.log($(highlightedLines[i]).text());
+					numMatchedLines++;
+					matchFound = true;
+					break;
+				}
+			}
+			if(matchFound === false) {
+				overHighlighted = true; // not in wantedLines => unwanted line
+			}
+		}
+		underHighlighted = (numMatchedLines < numWantedLines); // missed some lines we want highlighted
+		if(underHighlighted) {
+			errors.push("You've missed some lines included in the specified scope.");
+		}
+		if(overHighlighted) {
+			errors.push("You've added some lines that aren't actually in the specified scope.");
+		}
+
+		/* Checking free-response answers */
+		var userAnswers = $("#try-" + problemNumber + " input"); // array of input elements
+		var correctAnswers = currentProblem.freeResponseArray;
+		var incorrectAnswersFound = false;
+		if(userAnswers.length != correctAnswers.length) {
+			console.log("ERROR: mismatch in problem definitions for problem " + problemNumber + ".");
+		}
+		for(var i = 0; i < userAnswers.length; i++) {
+			if($(userAnswers[i]).val() == correctAnswers[i]) {
+				$(userAnswers[i]).css('color','green');
+			} else {
+				$(userAnswers[i]).css('color','red');
+				incorrectAnswersFound = true;
+			}
+		}
+		if(incorrectAnswersFound) {
+			errors.push("Some of your free response questions were incorrect.");
+		}
+
+		return errors;
+}
+
+/* Takes a list of errors and returns a string of formatted feedback for display. */
 function getFeedbackText(errors) {
 	var feedbackText = "";
 	if(errors.length != 0) {
-		feedbackText = "<ul>";
+		feedbackText = "<p class='msg-error'>Try again!</p><ul>";
 		for(var i = 0; i < errors.length; i++) {
 			feedbackText += "<li class='msg-error'>" + errors[i] + "</li>";
 		}
@@ -38,26 +107,19 @@ function getFeedbackText(errors) {
 	return feedbackText;
 }
 
-function getErrors(problemNumber) {
-	return ["fake error"];
+var assessmentProblems = [];
+
+/* Reads in a list of problem answer definitions and stores them in the global array assessmentProblems. */
+function getProblems() {
+	for (var i = 0; i < problemData.length; i++) {
+		var newProblem = new ProblemAnswer(problemData[i]);
+		assessmentProblems.push(newProblem);
+	}
 }
 
-function ProblemAnswer(problemNumber, lineArray, colorArray, freeResponseArray) {
-	this.problemNumber = problemNumber;
-	this.lineArray = lineArray;
-	this.colorArray = colorArray;
-	this.freeResponseArray = freeResponseArray;
-
-}
-
-function showValueOnClick(buttonName, boxName, value) {
-	console.log("here");
-	$(boxName).val(value);
-	$(buttonName).addClass("disabled");
-}
 
 $(document).ready(function() {
-	console.log("ready");
+	getProblems();
 	/* "What is scope" highlightable demo. */
 	$("#var-what-global").hover(function() { toggleColor("#scope-what-global", "cyan"); });
 	$("#var-what-arg").hover(function() { toggleColor("#scope-what-arg", "magenta"); });
@@ -97,7 +159,7 @@ $(document).ready(function() {
 	$("#check-local-copies-globalB").click(function() { showValueOnClick("#check-local-copies-globalB", "#show-local-copies-globalB", "3"); });
 	$("#check-local-copies-argB").click(function() { showValueOnClick("#check-local-copies-argB", "#show-local-copies-argB", "3"); });
 	$("#check-local-copies-globalC").click(function() { showValueOnClick("#check-local-copies-globalC", "#show-local-copies-globalC", "3"); });
-	$("#check-local-copies-argC").click(function() { showValueOnClick("#check-local-copies-argC", "#show-local-copies-argC", "-999"); });
+	$("#check-local-copies-argC").click(function() { showValueOnClick("#check-local-copies-argC", "#show-local-copies-argC", "5"); });
 	$("#check-local-copies-globalD").click(function() { showValueOnClick("#check-local-copies-globalD", "#show-local-copies-globalD", "3"); });
 	$("#check-local-copies-argD").click(function() { showValueOnClick("#check-local-copies-argD", "#show-local-copies-argD", "nonexistent!"); });
 
@@ -119,42 +181,19 @@ $(document).ready(function() {
 	$("#var-local-block-false").hover(function() { toggleColor("#scope-local-block-false", "cyan"); });
 	$("#check-local-block-false").click(function() { showValueOnClick("#check-local-block-false", "#show-local-block-false", "undefined"); });
 
+	/* Code hover-highlighting for the different assessment problems. Using yellow instead of cyan and magenta (previously associated with global/local.) */
+	$("#try .highlightable").click(function() { $(this).toggleClass("yellow"); });
 
-	$("#try-1 .highlightable").click(function() { $(this).toggleClass("magenta"); });
-
+	/* Setting up evaluation and feedback for user submissions on assessment problems. */
 	$("#try button").click(function() {
 		var problemName = $(this).parent().parent().attr("id");
 		var problemNumber = problemName[problemName.length - 1];
-		// TODO: FIX
-		// $($("#try-1 .well span.highlightable")[4]).text()
-		// $($("#try-1 .well .magenta")[0])
-		var errors = getErrors(problemNumber);
+
+		var errors = getProblemErrors(problemNumber)
 		var feedback = getFeedbackText(errors);
-		console.log(feedback);
 		$("#feedback-" + problemNumber).html(feedback);
 	});
 
-	console.log("done");
-    //$("ol").sortable();
-    //$(".test").css("background-color","#00FFFF");
-    //highlightCyan(".scope-A");
+	/* User free answer are colored red/green upon evaluation, so turn the text black again if they try anything else. */
+	$("#try input").keypress(function() { $(this).css('color','black'); });
 });
-
-/*
-$(document).ready(function() {
-	$(".var-A").hover(function() {
-		$(".scope-A").css("background-color","#00FFFF");
-	}, function() {
-		$(".scope-A").css("background-color","transparent");
-	});
-    //$("ol").sortable();
-});
-
-function highlightCyan(target) {
-	$(".scope-A").css("background-color","#00FFFF");
-}
-
-function revertHighlight(target) {
-	$(".scope-A").css("background-color","transparent");
-}
-*/
